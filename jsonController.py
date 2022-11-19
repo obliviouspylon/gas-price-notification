804ed8fafd856331c2ca357e3165a3205054834f
James Luo <contact@jamesckluo.ca>
2022-11-19 03:39:18 -0500

    Add json data file

---
+++ jsonController.py
@@ -0,0 +1,44 @@
+
+import json
+import datetime
+
+def checkJson(readData = False):
+    # today = datetime.datetime.now().strftime("%Y%m%d")
+    tomrorow = (datetime.datetime.now() + datetime.timedelta(days=1)).strftime("%Y%m%d")
+    yesterday = (datetime.datetime.now() - datetime.timedelta(days=1)).strftime("%Y%m%d")
+
+    write = False
+    try:
+        with open("predictionData.json","r") as f:
+            data = json.load(f)
+        if yesterday in data:
+            data.pop(yesterday)
+            write = True
+    except:
+        data = {}
+        write = True
+
+    if not tomrorow in data:
+        data[tomrorow] = {}
+
+    if write:
+        with open("predictionData.json","w") as f:
+            json.dump(data, f, indent=4)
+    
+    if readData:
+        return (data, tomrorow)
+        
+def saveJson(website, direction, amount, price):
+    data, tomorrow = checkJson(readData = True)
+
+    if not website in data[tomorrow]:
+        data[tomorrow][website] = {
+            "direction" : direction,
+            "amount" : amount,
+            "price" : price
+        }
+    try:
+        with open("predictionData.json","w") as f:
+            json.dump(data, f, indent=4)
+    except:
+        pass
--- server.py
+++ server.py
@@ -1,7 +1,11 @@
-from flask import Flask
+from flask import Flask, Response
 from flask_cors import CORS
+from flask_apscheduler import APScheduler # https://viniciuschiele.github.io/flask-apscheduler/rst/usage.html https://www.techcoil.com/blog/how-to-use-flask-apscheduler-in-your-python-3-flask-application-to-run-multiple-tasks-in-parallel-from-a-single-http-request/
 from waitress import serve
-import gasPrice
+import datetime
+import gasWizard
+import Enpro680
+import jsonController
 
 app = Flask(__name__)
 CORS(app)
@@ -10,13 +14,38 @@ CORS(app)
 port = 9000
 listenOn = "0.0.0.0"
 
+def getGasPrediction():
+    result = gasWizard.getPrediction()
+    if result[0]:
+        jsonController.saveJson("GasWizard",result[1][0],result[1][1],result[1][2])
+    result = Enpro680.getPrediction()
+    if result[0]:
+        jsonController.saveJson("EnPro",result[1][0],result[1][1],result[1][2])
 
 @app.route('/')
-def gasPrediction():
-    message = gasPrice.getPrediction()
-    return(message)
+def sendPrediction():
+    # message = gasWizard.getPrediction()
+    data, tomorrow = jsonController.checkJson(readData=True)
+    tomorrowDate = datetime.datetime.strptime(tomorrow, "%Y%m%d")
+    message = ""
+
+    for site in data[tomorrow]:
+        direction = data[tomorrow][site]["direction"]
+        amount = str(data[tomorrow][site]["amount"])
+        price = str(data[tomorrow][site]["price"])
+        if direction == "STAYS":
+            message = message + site + ": " + direction + " at " + price + "\n"
+        else:
+            message = message + site + ": " + direction + " by " + amount + " to " + price + "\n"
+
+    if message == "":
+        return Response("No prediction found for " + tomorrowDate.strftime("%b %d"), status=201)
+    else:
+        message = "Gas Prediction - " + tomorrowDate.strftime("%b %d") + "\n" + message
+        return(message[:-1])
 
 if __name__ == '__main__':
+    # print(sendPrediction())
     print("Starting Flask Server...")
     print("Listening on " + listenOn + ":" + str(port))
     serve(app,host = listenOn, port = port)
