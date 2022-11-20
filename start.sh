8c218a1b599703df9453fad76503d5821784e210
James Luo <contact@jamesckluo.ca>
2022-11-20 23:25:22 +0000

    Added cd to repo path so the service isn't trying to create a file in a root permission folder

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 source /home/ubuntu/gas-price-notification/venv/bin/activate
-python3 /home/ubuntu/gas-price-notification/server.py
+cd /home/ubuntu/gas-price-notification
+python3 server.py
 echo 'Gas Price Notification Service Started'
 
